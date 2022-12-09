f8885a17accb151bf6f50e78adde70f5ade51c76
khalidilmi <khalidaxmedilmi@gmail.com>
2022-12-09 13:25:27 +0100

    commit

---
+++ Tesla/Data/BilData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Tesla.Models;
+
+namespace Tesla.Data
+
+
+{
+    public class BilData:Bil
+    {
+        private static List<Bil> _bil = new List<Bil>()
+        {
+            new Bil("y",5499,"den bedste bil"),
+            new Bil("x",5499,"den bedste bil"),
+            new Bil("z",1899,"den bedste bil"),
+            new Bil("y",1599,"den bedste bil"),
+            new Bil("x",9999,"den bedste bil"),
+            new Bil("z",9599,"den bedste bil"),
+        };
+    }
+}
