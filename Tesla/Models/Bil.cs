577e5d81d002b70504d56cd1cb3f7bf6987bc9e5
Gandalf2630 <durmus2630@gmail.com>
2022-12-09 13:04:47 +0100

    Added CarAge.

--- Tesla/Models/Bil.cs
+++ Tesla/Models/Bil.cs
@@ -1,21 +1,24 @@
-﻿namespace Tesla.Models
+﻿using System.Net.Cache;
+
+namespace Tesla.Models
 {
     public class Bil
     {
         public string Model { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
-
+        public int CarAge { get; set; }
         public Bil ()
         {
 
         }
 
-        public Bil (string model, double price, string description)
+        public Bil (string model, double price, string description, int carage)
         {
             Model = model;
             Price = price;
             Description = description;
+            CarAge = carage;
         }
 
         public override string ToString()
