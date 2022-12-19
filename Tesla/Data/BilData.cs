56e9624b5c278faf1a96f25d13ebb44512bf674d
khalidilmi <khalidaxmedilmi@gmail.com>
2022-12-19 10:38:00 +0100

    tilfæjet de forskellige biler

--- Tesla/Data/BilData.cs
+++ Tesla/Data/BilData.cs
@@ -5,16 +5,16 @@ namespace Tesla.Data
 
 
 {
-    public class BilData:Bil
-    {
-        private static List<Bil> _bil = new List<Bil>()
-        {
-            new Bil("y",5499,"den bedste bil"),
-            new Bil("x",5499,"den bedste bil"),
-            new Bil("z",1899,"den bedste bil"),
-            new Bil("y",1599,"den bedste bil"),
-            new Bil("x",9999,"den bedste bil"),
-            new Bil("z",9599,"den bedste bil"),
-        };
-    }
+    //public class BilData:Bil
+    //{
+    //    private static List<Bil> _bil = new List<Bil>()
+    //    {
+    //        new Bil("y",5499,"den bedste bil"),
+    //        new Bil("x",5499,"den bedste bil"),
+    //        new Bil("z",1899,"den bedste bil"),
+    //        new Bil("y",1599,"den bedste bil"),
+    //        new Bil("x",9999,"den bedste bil"),
+    //        new Bil("z",9599,"den bedste bil"),
+    //    };
+    //}
 }
